8f6de59a747ff31f93655081709dd73c714175a7
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-11-11 11:15:23 -0500

    made code

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Pablo Carreras
+ * Created on: Nov 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(13)
+let distanceOfObject: number
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    //distanceOfObject
+    distanceOfObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    // if distance is greater than 10
+    if (distanceOfObject < 10) {
+        basic.showIcon(IconNames.Triangle)
+        radio.sendString("Too close!")
+        basic.showIcon(IconNames.Happy)
+    }
+})
+
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
